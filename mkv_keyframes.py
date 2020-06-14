75ae4d32a9f5960e18d65961eb2502fc159ab7e8
w <wvvwvvvvwvvw@gmail.com>
2020-06-13 21:53:36 -0700

    segment correcting

--- mkv_keyframes.py
+++ mkv_keyframes.py
@@ -1,9 +1,11 @@
 import subprocess, re
 
-def get_mkv_keyframes(src, total_frames, cb=None):
+# returns list of keyframes, total_frames
+def get_mkv_keyframes(src, cb=None):
   ff = [
     "ffmpeg", "-hide_banner",
     "-i", src,
+    "-map", "0:v:0",
     "-vf", "select=eq(pict_type\,PICT_TYPE_I)",
     "-f", "null",
     "-loglevel", "debug", "-"
@@ -14,6 +16,7 @@ def get_mkv_keyframes(src, total_frames, cb=None):
     stderr=subprocess.STDOUT)
 
   mkv_keyframes = []
+  total_frames = 0
 
   while True:
     line = ffmpeg_pipe.stdout.readline().strip().decode("utf-8")
@@ -25,7 +28,10 @@ def get_mkv_keyframes(src, total_frames, cb=None):
     if match:
       frame = int(match.group(1))
       mkv_keyframes.append(frame)
-      if cb:
-        cb(frame)
+      if cb: cb(frame)
+    else:
+      match = re.search(r"video.+?([0-9]+?) frames decoded", line)
+      if match:
+        total_frames = int(match.group(1))
 
-  return mkv_keyframes
+  return mkv_keyframes, total_frames
--- split.py
+++ split.py
@@ -3,6 +3,7 @@ from util import get_frames, ffmpeg
 from mkv_keyframes import get_mkv_keyframes
 from aom_keyframes import get_aom_keyframes
 
+# returns splits, total frames, segments
 # splits are contained like so:
 # {
 #   "00000": {                # aom segment
@@ -11,31 +12,41 @@ from aom_keyframes import get_aom_keyframes
 #     "frames": 5             # number of frames for the aom segment
 #   }
 # }
-def split(video, path_split, total_frames, min_frames=-1, max_frames=-1, cb=None):
+# segments are contained like so:
+# {
+#   "00000.mkv": {
+#     "start": 0,
+#     "length": 10
+#   }
+# }
+def split(video, path_split, min_frames=-1, max_frames=-1, cb=None):
+  mkv_keyframes, total_frames = get_mkv_keyframes(video)
+  mkv_keyframes.append(total_frames)
+
   aom_keyframes = get_aom_keyframes(video)
   
+  skip_keyframes = 0
+
   if min_frames != -1 and max_frames != -1:
     final_scenes = []
     last_scene = 0
-    previous_scene = aom_keyframes[1]
-    for scene in aom_keyframes[1:]:
+    previous_scene = aom_keyframes[skip_keyframes]
+    for scene in aom_keyframes[skip_keyframes + 1:]:
       if scene - last_scene >= max_frames and previous_scene - last_scene > min_frames:
         final_scenes.append(previous_scene)
         last_scene = previous_scene
       previous_scene = scene
-    aom_keyframes = [0] + final_scenes + [total_frames]
-  else:
-    aom_keyframes.append(total_frames)
-
-  mkv_keyframes = get_mkv_keyframes(video, total_frames, cb) + [total_frames]
-
+    aom_keyframes = aom_keyframes[:skip_keyframes + 1] + final_scenes
+    
+  aom_keyframes.append(total_frames)
+  
   splits = {}
   last_end = 0
   frames = []
 
   for i in range(len(aom_keyframes) - 1):
     frame = aom_keyframes[i]
-    next_frame = aom_keyframes[i + 1]
+    next_frame = aom_keyframes[i+1]
     segment_n = len(frames)
     start = 0
     length = next_frame - frame
@@ -51,18 +62,20 @@ def split(video, path_split, total_frames, min_frames=-1, max_frames=-1, cb=None
       start = frame - largest
       if largest in frames or largest < last_end:
         segment_n -= 1
-        start = frame - frames[len(frames) - 1]
+        start = frame - frames[len(frames)-1]
       else:
         frames.append(largest)
-        
-      print(segment_n, start, length)
       
-    splits[f"{len(splits):05d}"] = {
-      "segment": f"{segment_n:05d}.mkv",
-      "start": start,
-      "frames": length
-    }
+    splits[f"{len(splits):05d}"] = ({"segment": f"{segment_n:05d}.mkv", "start": start, "frames": length, "filesize": 0})
     last_end = frame + length
+
+  segments = {}
+
+  for segment_n in range(len(frames)):
+    segments[f"{segment_n:05d}.mkv"] = {
+      "start": frames[segment_n],
+      "length": (total_frames if segment_n == len(frames) - 1 else frames[segment_n + 1]) - frames[segment_n]
+    }
 
   frames = [str(f) for f in frames][1:]
 
@@ -84,10 +97,39 @@ def split(video, path_split, total_frames, min_frames=-1, max_frames=-1, cb=None
   cmd.append(os.path.join(path_split, "%05d.mkv"))
 
   os.makedirs(path_split, exist_ok=True)
+  ffmpeg(cmd, lambda x: cb(x, total_frames))
 
-  ffmpeg(cmd, cb)
+  return splits, total_frames, segments
 
-  return splits
+def correct_split(path_in, path_out, start, length):
+  cmd = [
+    "ffmpeg", "-hide_banner",
+    "-i", path_in,
+    "-map", "0:v:0",
+    "-c:v", "libx265",
+    "-x265-params", "lossless=1",
+    "-vf", f"select=gte(n\\,{start})",
+    "-frames:v", str(length),
+    "-y", path_out
+  ]
+  ffmpeg(cmd, lambda x: print(f"{x}/{length}", end="\r"))
+
+# input the source and segments produced by split()
+def verify_split(path_in, path_split, segments, cb=None):
+  for i, segment in enumerate(segments, start=1):
+    print(segment)
+    segment_n = str(os.path.splitext(segment)[0])
+    num_frames = get_frames(os.path.join(path_split, segment))
+    if num_frames != segments[segment]["length"]:
+      print("bad framecount", segment, "expected:", segments[segment]["length"], "got:", num_frames)
+      correct_split(path_in, os.path.join(path_split, segment), segments[segment]["start"], segments[segment]["length"])
+    else:
+      num_frames_slow = get_frames(os.path.join(path_split, segment), False)
+      if num_frames != num_frames_slow:
+        print("bad framecount", segment, "expected:", num_frames, "got:", num_frames_slow)
+        correct_split(path_in, os.path.join(path_split, segment), segments[segment]["start"], segments[segment]["length"])
+    
+    if cb: cb(i)
 
 # this is an example program
 if __name__ == "__main__":
@@ -102,15 +144,22 @@ if __name__ == "__main__":
   
   args = parser.parse_args()
 
-  total_frames = get_frames(args.input)
-  print(f"{total_frames} frames")
-
-  splits = split(
+  splits, total_frames, segments = split(
     args.input,
     args.split_path,
-    total_frames,
     min_frames=args.min_frames,
     max_frames=args.max_frames,
-    cb=lambda x: print(f"{x}/{total_frames}", end="\r"))
+    cb=lambda x: print(f"{x}/{total_frames}", end="\r")
+  )
+  
+  print(total_frames, "frames")
+  print("verifying split")
+
+  verify_split(
+    args.input,
+    args.split_path,
+    segments,
+    cb=lambda x: print(f"{x}/{len(segments)}", end="\r")
+  )
 
   json.dump(splits, open(args.splits, "w+"))
