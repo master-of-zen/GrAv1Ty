b1f38eab6e34ef031f07d2a3228c49952ccf63fc
w <wvvwvvvvwvvw@gmail.com>
2020-06-17 17:09:09 -0700

    alternatively re-encode if keyframes don't align

--- split.py
+++ split.py
@@ -22,10 +22,10 @@ from aom_keyframes import get_aom_keyframes
 def split(video, path_split, min_frames=-1, max_frames=-1, cb=None):
   mkv_keyframes, total_frames = get_mkv_keyframes(video)
   mkv_keyframes.append(total_frames)
-
-  aom_keyframes = get_aom_keyframes(video)
   
   skip_keyframes = 0
+
+  aom_keyframes = get_aom_keyframes(video)
 
   if min_frames != -1 and max_frames != -1:
     final_scenes = []
@@ -37,12 +37,74 @@ def split(video, path_split, min_frames=-1, max_frames=-1, cb=None):
         last_scene = previous_scene
       previous_scene = scene
     aom_keyframes = aom_keyframes[:skip_keyframes + 1] + final_scenes
-    
+  
   aom_keyframes.append(total_frames)
   
+  cmd = [
+    "ffmpeg", "-y",
+    "-i", video,
+    "-map", "0:v:0",
+    "-avoid_negative_ts", "1"
+  ]
+
+  frames, splits, segments = partition_with_mkv(aom_keyframes, mkv_keyframes, total_frames)
+  reencode = False
+  if len(frames) < len(aom_keyframes) / 2:
+    splits = {}
+    frames = []
+    segments = {}
+
+    print("keyframes unreliable, re-encoding")
+
+    for i in range(len(aom_keyframes) - 1):
+      frame = aom_keyframes[i]
+      next_frame = aom_keyframes[i+1]
+      segment_n = len(frames)
+      length = next_frame - frame
+      frames.append(frame)
+      splits[f"{len(splits):05d}"] = ({
+        "segment": f"{segment_n:05d}.mkv",
+        "start": 0,
+        "frames": length,
+        "filesize": 0
+      })
+      segments[f"{segment_n:05d}.mkv"] = {
+        "start": frame,
+        "length": length
+      }
+
+    reencode = True
+
+  frames = [str(f) for f in frames][1:]
+  print(frames)
+
+  if reencode:
+    cmd.extend([
+      "-c:v", "libx264",
+      "-crf", "0",
+      "-x264-params", "scenecut=0",
+      "-force_key_frames", "expr:" + "+".join([f"eq(n,{int(f)})" for f in frames])
+    ])
+  else:
+    cmd.extend(["-c", "copy"])
+
+  cmd.extend([
+    "-f", "segment",
+    "-segment_frames", ",".join(frames)
+  ])
+
+  cmd.append(os.path.join(path_split, "%05d.mkv"))
+
+  os.makedirs(path_split, exist_ok=True)
+  ffmpeg(cmd, lambda x: cb(x, total_frames))
+
+  return splits, total_frames, segments
+
+def partition_with_mkv(aom_keyframes, mkv_keyframes, total_frames):
   splits = {}
   last_end = 0
   frames = []
+  segments = {}
 
   for i in range(len(aom_keyframes) - 1):
     frame = aom_keyframes[i]
@@ -65,49 +127,27 @@ def split(video, path_split, min_frames=-1, max_frames=-1, cb=None):
         start = frame - frames[len(frames)-1]
       else:
         frames.append(largest)
-      
+    
     splits[f"{len(splits):05d}"] = ({"segment": f"{segment_n:05d}.mkv", "start": start, "frames": length, "filesize": 0})
     last_end = frame + length
-
-  segments = {}
-
+  
   for segment_n in range(len(frames)):
     segments[f"{segment_n:05d}.mkv"] = {
       "start": frames[segment_n],
       "length": (total_frames if segment_n == len(frames) - 1 else frames[segment_n + 1]) - frames[segment_n]
     }
 
-  frames = [str(f) for f in frames][1:]
-
-  cmd = [
-    "ffmpeg", "-y",
-    "-i", video,
-    "-map", "0:v:0",
-    "-an",
-    "-c", "copy",
-    "-avoid_negative_ts", "1"
-  ]
-
-  if len(frames) > 0:
-    cmd.extend([
-      "-f", "segment",
-      "-segment_frames", ",".join(frames)
-    ])
-
-  cmd.append(os.path.join(path_split, "%05d.mkv"))
-
-  os.makedirs(path_split, exist_ok=True)
-  ffmpeg(cmd, lambda x: cb(x, total_frames))
-
-  return splits, total_frames, segments
+  return frames, splits, segments
 
 def correct_split(path_in, path_out, start, length):
   cmd = [
     "ffmpeg", "-hide_banner",
     "-i", path_in,
     "-map", "0:v:0",
-    "-c:v", "libx265",
-    "-x265-params", "lossless=1",
+    "-c:v", "libx264",
+    "-crf", "0",
+    "-force_key_frames", f"expr:eq(n,{start})",
+    "-x264-params", "scenecut=0",
     "-vf", f"select=gte(n\\,{start})",
     "-frames:v", str(length),
     "-y", path_out
@@ -118,16 +158,18 @@ def correct_split(path_in, path_out, start, length):
 def verify_split(path_in, path_split, segments, cb=None):
   for i, segment in enumerate(segments, start=1):
     print(segment)
+    path_segment = os.path.join(path_split, segment)
     segment_n = str(os.path.splitext(segment)[0])
-    num_frames = get_frames(os.path.join(path_split, segment))
+    num_frames = get_frames(path_segment)
+
     if num_frames != segments[segment]["length"]:
       print("bad framecount", segment, "expected:", segments[segment]["length"], "got:", num_frames)
-      correct_split(path_in, os.path.join(path_split, segment), segments[segment]["start"], segments[segment]["length"])
+      correct_split(path_in, path_segment, segments[segment]["start"], segments[segment]["length"])
     else:
-      num_frames_slow = get_frames(os.path.join(path_split, segment), False)
+      num_frames_slow = get_frames(path_segment, False)
       if num_frames != num_frames_slow:
         print("bad framecount", segment, "expected:", num_frames, "got:", num_frames_slow)
-        correct_split(path_in, os.path.join(path_split, segment), segments[segment]["start"], segments[segment]["length"])
+        correct_split(path_in, path_segment, segments[segment]["start"], segments[segment]["length"])
     
     if cb: cb(i)
 
@@ -151,7 +193,7 @@ if __name__ == "__main__":
     max_frames=args.max_frames,
     cb=lambda x: print(f"{x}/{total_frames}", end="\r")
   )
-  
+
   print(total_frames, "frames")
   print("verifying split")
 
