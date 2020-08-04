075e5b604a1c2b36e66b766b9c48aa8824112bb4
w <wvvwvvvvwvvw@gmail.com>
2020-08-04 13:54:11 -0700

    Update plot_vmaf.py

--- plot_vmaf.py
+++ plot_vmaf.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python3
 
-import sys, subprocess, os
+import subprocess, os, json, re
 import numpy as np
 import matplotlib.pyplot as plt
+import xml.etree.ElementTree as Xml
 
-# https://github.com/master-of-zen/Plot_Vmaf/blob/master/plot_vmaf.py
-def read_vmaf_xml(file, out, title, dpi):
-  with open(file, "r") as f:
-    file = f.readlines()
-    file = [x.strip() for x in file if "vmaf=\"" in x]
-    vmafs = []
-    for i in file:
-      vmf = i[i.rfind("=\"") + 2: i.rfind("\"")]
-      vmafs.append(float(vmf))
+def read_vmaf_xml(file):
+  root = Xml.parse(file).getroot()
+  frames = []
+  header = ["frame", "vmaf"]
 
-    vmafs = [round(float(x), 3) for x in vmafs if type(x) == float]
+  for child in root:
+    if child.tag == "frames":
+      for frame in child:
+        frame_n = int(frame.get("frameNum"))
+        vmaf = float(frame.get("vmaf"))
+        psnr = float(frame.get("psnr"))
+        ssim = float(frame.get("ssim"))
+        ms_ssim = float(frame.get("ms_ssim"))
 
-  # Data
-  x = [x for x in range(len(vmafs))]
-  mean = round(sum(vmafs) / len(vmafs), 3)
-  perc_1 = round(np.percentile(vmafs, 1), 3)
-  perc_25 = round(np.percentile(vmafs, 25), 3)
-  perc_75 = round(np.percentile(vmafs, 75), 3)
+        row = [frame_n, vmaf]
 
-  # Plot
-  plt.figure(figsize=(15, 4))
-  if title: plt.title(title)
-  [plt.axhline(i, color="grey", linewidth=0.4) for i in range(0, 100)]
-  [plt.axhline(i, color="black", linewidth=0.6) for i in range(0, 100, 5)]
-  plt.plot(x, vmafs, label=f"Frames: {len(vmafs)}\nMean: {mean}\n1%: {perc_1}\n25%: {perc_25}\n75%: {perc_75}", linewidth=0.4)
-  plt.ylabel("VMAF")
-  plt.xlabel("Frame")
-  plt.legend(loc="lower right")
-  plt.ylim(min(vmafs), 100)
-  plt.tight_layout()
-  plt.margins(0)
+        if psnr:
+          if "psnr" not in header:
+            header.append("psnr")
+          row.append(psnr)
+        if ssim:
+          if "ssim" not in header:
+            header.append("ssim")
+          row.append(ssim)
+        if ms_ssim:
+          if "ms_ssim" not in header:
+            header.append("ms_ssim")
+          row.append(ms_ssim)
 
-  plt.savefig(out, dpi=dpi)
+        frames.append(row)
 
-def calculate(source, encoded, frames, vmaf, title, dpi, output, out_dir, open_image):
+  return header, frames
+
+def create_log(source, encoded, frames, vmaf, extra_metrics=[], xml="plot.xml"):
+  extra = ":".join(extra_metrics)
+  extra += ":" if len(extra) > 0 else ""
+
   ffmpeg = ["ffmpeg",
     "-hide_banner",
     "-r", "24",
@@ -47,7 +50,7 @@ def calculate(source, encoded, frames, vmaf, title, dpi, output, out_dir, open_i
     "-i", source,
     "-map", "0:v:0",
     "-map", "1:v:0",
-    "-lavfi", "libvmaf=log_path=plot.xml" + f":model_path={vmaf}" if vmaf else "",
+    "-lavfi", f"libvmaf={extra}log_path={xml}" + f":model_path={vmaf}" if vmaf else "",
   ]
 
   if frames:
@@ -57,75 +60,137 @@ def calculate(source, encoded, frames, vmaf, title, dpi, output, out_dir, open_i
     "-f", "null", "-"
   ])
 
-  subprocess.run(ffmpeg)
+  pipe = subprocess.Popen(ffmpeg, 
+      stdout=subprocess.PIPE,
+      stderr=subprocess.STDOUT,
+      universal_newlines=True)
   
-  if output and out_dir:
-    os.makedirs(output, exist_ok=True)
-    out = os.path.join(output, f"{os.path.basename(encoded)}.png")
-  elif output:
-    out = output
-  else:
-    out = f"{os.path.basename(encoded)}.png"
+  while True:
+    line = pipe.stdout.readline().strip()
 
-  read_vmaf_xml("plot.xml", out, title, dpi)
+    if len(line) == 0 and pipe.poll() is not None:
+      break
 
-  if open_image:
-    Image.open(out).show()
+    match = re.search(r"frame= +?([0-9]+?) ", line)
+    if match:
+      frames = int(match.group(1))
+      print(f"frame {frames}", end="\r")
 
-# --open requires Pillow
-# example use
-# python3 plot_vmaf.py raw.mkv encoded.mkv --frames 10 --open
-# python3 plot_vmaf.py raw.mkv encoded.mkv --frames 10 -o image.png --open
-# python3 plot_vmaf.py raws/ encoded/ --frames 10 -o vmafs
+def calculate(xml, output=None, png=False, svg=False, csv=False, psnr=False, ssim=False, ms_ssim=False):
+  header, frames = read_vmaf_xml(xml)
+
+  if not output:
+    return header, frames
+
+  frames = [",".join([str(f) for f in frame]) for frame in frames]
+  if csv:
+    header = ",".join(header)
+    open(f"{output}.csv", "w+").write(f"{header}\n" + "\n".join(frames))
+
+# examples
+# python3 plot_vmaf.py --xml plot.xml -o metrics.csv --psnr --ssim -ms_ssim
+# python3 plot_vmaf.py raw.mkv encoded.mkv --frames 10 -o metrics.csv
 if __name__ == "__main__":
   import argparse
 
   parser = argparse.ArgumentParser()
-  parser.add_argument(dest="source")
-  parser.add_argument(dest="encoded")
-  parser.add_argument("--title", default=None)
+
+  parser.add_argument("source", nargs="?")
+  parser.add_argument("encoded", nargs="?")
+
   parser.add_argument("--frames", default=None)
-  parser.add_argument("--dpi", default=100)
   parser.add_argument("-o", dest="output", default=None)
-  parser.add_argument("--open", action="store_true")
   parser.add_argument("--vmaf-model-path", dest="vmaf_path", default="vmaf_v0.6.1.pkl" if os.name == "nt" else None)
-  
+
+  parser.add_argument("--psnr", action="store_true")
+  parser.add_argument("--ssim", action="store_true")
+  parser.add_argument("--ms_ssim", action="store_true")
+
+  #parser.add_argument("--png", action="store_true")
+  #parser.add_argument("--svg", action="store_true")
+  parser.add_argument("--csv", action="store_true")
+
+  #parser.add_argument("--title", default=None)
+  #parser.add_argument("--dpi", default=100)
+
+  parser.add_argument("--xml", default=None)
+
   args = parser.parse_args()
 
-  if args.open:
-    from PIL import Image
+  if not args.xml or (args.source or args.encoded):
+    missing = []
+    if not args.source:
+      missing.append("source")
+    if not args.encoded:
+      missing.append("encoded")
+    
+    if missing:
+      parser.error("the following arguments are required: " + ", ".join(missing))
+
+  elif not args.source and not args.encoded:
+    if not os.path.isfile(args.xml):
+      parser.error(f"{args.xml} could not be found")
+
+  extra_metrics = []
+
+  if args.psnr:
+    extra_metrics.append("psnr")
+
+  if args.ssim:
+    extra_metrics.append("ssim")
+
+  if args.ms_ssim:
+    extra_metrics.append("ms_ssim")
+
+  print("metrics:", ", ".join(["vmaf"] + extra_metrics))
+
+  extra_metrics = [f"{m}=1" for m in extra_metrics]
 
   source = args.source
   encoded = args.encoded
 
-  if os.path.isdir(source) and os.path.isdir(encoded):
-    files_in_source = os.listdir(source)
-    files_in_encoded = os.listdir(encoded)
-    for file in files_in_encoded:
-      if file in files_in_source:
-        calculate(
-          os.path.join(source, file),
-          os.path.join(encoded, file),
-          args.frames,
-          args.vmaf_path,
-          args.title,
-          args.dpi,
-          args.output,
-          True,
-          args.open)
-  elif os.path.isdir(source) or os.path.isdir(encoded):
+  if source and encoded:
     if os.path.isdir(source):
-      print(os.path.isdir(source), "is a directory and", os.path.isdir(encoded), "is not")
+      parser.error(f"{source} is a directory")
     if os.path.isdir(encoded):
-      print(os.path.isdir(encoded), "is a directory and", os.path.isdir(source), "is not")
-  else:    
-    calculate(
+      parser.error(f"{encoded} is a directory")
+  
+  #if not args.output and not args.png and not args.svg and not args.csv:
+  if not args.output and not args.csv:
+    #parser.error("An output file or output types --png, --svg, and --csv must be provided")
+    parser.error("An output file or output types --csv must be provided")
+
+  out_filename, ext = os.path.splitext(args.output)
+  ext = ext.lower()
+
+  #if ext == ".png":
+  #  args.png = True
+  #if ext == ".svg":
+  #  args.svg = True
+  if ext == ".csv":
+    args.csv = True
+  
+  if ext in [".png", ".svg", ".csv"]:
+    args.output = out_filename
+  #elif not args.png and not args.svg and not args.csv:
+  elif not args.csv:
+    #parser.error("Could not detect output file type\nSpecify --png, --svg, or --csv")
+    parser.error("Could not detect output file type\nSpecify --csv")
+
+  if source and encoded:
+    create_log(
       source,
       encoded,
       args.frames,
       args.vmaf_path,
-      args.title,
-      args.dpi,
-      args.output,
-      False,
-      args.open)
+      extra_metrics,
+      args.xml if args.xml else "plot.xml"
+    )
+
+  calculate(
+    args.xml if args.xml else "plot.xml", args.output,
+    #args.png, args.svg, args.csv,
+    False, False, args.csv,
+    args.psnr, args.ssim, args.ms_ssim,
+    args.title, args.dpi
+  )
